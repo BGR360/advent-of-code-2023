36494457ae1cf6e79a9c62085456d0e6605c451e
Ben Reeves <benwolverine2019@gmail.com>
2023-12-01 23:59:30 -0600

    Day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,28 +1,86 @@
 advent_of_code::solution!(1);
 
-fn get_number_from_line(line: &str) -> u32 {
-    //let line = line.as_bytes();
-    let first_digit = line
-        .chars()
-        .find(|&c| c.is_ascii_digit())
-        .expect("there should be a first digit");
-    let last_digit = line
-        .chars()
-        .rfind(|&c| c.is_ascii_digit())
-        .expect("there should be a last digit");
-
-    let first_digit = first_digit.to_digit(10).unwrap();
-    let last_digit = last_digit.to_digit(10).unwrap();
-
-    (first_digit * 10) + last_digit
-}
+use advent_of_code::debugln;
+use nom::{branch::alt, bytes::complete::tag, character::complete::satisfy, IResult, Parser};
 
 pub fn part_one(input: &str) -> Option<u32> {
+    fn get_number_from_line(line: &str) -> u32 {
+        //let line = line.as_bytes();
+        let first_digit = line
+            .chars()
+            .find(|&c| c.is_ascii_digit())
+            .expect("there should be a first digit");
+        let last_digit = line
+            .chars()
+            .rfind(|&c| c.is_ascii_digit())
+            .expect("there should be a last digit");
+
+        let first_digit = first_digit.to_digit(10).unwrap();
+        let last_digit = last_digit.to_digit(10).unwrap();
+
+        (first_digit * 10) + last_digit
+    }
+
     Some(input.lines().map(get_number_from_line).sum())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn ascii_digit(input: &str) -> IResult<&str, u32> {
+    // surprisingly difficult to figure out how to take just a single character matching a predicate
+    satisfy(|c| c.is_ascii_digit())
+        .map(|digit| {
+            digit
+                .to_digit(10)
+                .expect("already validated that it's a digit")
+        })
+        .parse(input)
+}
+
+fn english_digit(input: &str) -> IResult<&str, u32> {
+    alt((
+        tag("one").map(|_| 1),
+        tag("two").map(|_| 2),
+        tag("three").map(|_| 3),
+        tag("four").map(|_| 4),
+        tag("five").map(|_| 5),
+        tag("six").map(|_| 6),
+        tag("seven").map(|_| 7),
+        tag("eight").map(|_| 8),
+        tag("nine").map(|_| 9),
+    ))(input)
+}
+
+fn digit(input: &str) -> IResult<&str, u32> {
+    alt((ascii_digit, english_digit))(input)
+}
+
+fn get_number_from_line(mut line: &str) -> u32 {
+    debugln!("{line}");
+
+    let mut digits = Vec::new();
+
+    while !line.is_empty() {
+        if let Ok((_, digit)) = digit(line) {
+            digits.push(digit);
+        }
+
+        // not kosher for unicode, but the puzzle input is ascii only :)
+        line = &line[1..];
+    }
+
+    debugln!("{digits:?}");
+
+    let first_digit = digits.first().expect("there should be a first digit");
+    let last_digit = digits.last().expect("there should be a last digit");
+
+    let number = (first_digit * 10) + last_digit;
+
+    debugln!("{number}");
+
+    number
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(input.lines().map(get_number_from_line).sum())
 }
 
 #[cfg(test)]
@@ -31,13 +89,22 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(281));
+    }
+
+    #[test]
+    fn test_trickies() {
+        assert_eq!(part_two("eighthree"), Some(83));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,19 @@ mod day;
 pub mod template;
 
 pub use day::*;
+
+#[macro_export]
+macro_rules! debug {
+    ($($args:expr),*) => {{
+        #[cfg(debug_assertions)]
+        print!($($args),*);
+    }};
+}
+
+#[macro_export]
+macro_rules! debugln {
+    ($($args:expr),*) => {{
+        #[cfg(debug_assertions)]
+        println!($($args),*);
+    }};
+}
