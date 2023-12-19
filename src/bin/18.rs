40b80368fe1b527532f3f2f4520d897c07685e55
Ben Reeves <benwolverine2019@gmail.com>
2023-12-18 18:24:15 -0600

    Day 18 part 2 first attempt

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,5 +1,7 @@
 advent_of_code::solution!(18);
 
+use std::collections::btree_map::Range;
+
 use advent_of_code::{
     debugln,
     helpers::grid::{self, Dir, Grid, Pos, Side},
@@ -12,18 +14,53 @@ pub struct DigPlan {
     pub steps: Vec<DigStep>,
 }
 
+impl DigPlan {
+    pub fn into_alt(self) -> Self {
+        Self {
+            steps: self.steps.into_iter().map(DigStep::into_alt).collect(),
+        }
+    }
+
+    pub fn min_and_max_positions(&self) -> (Pos, Pos) {
+        let mut pos = Pos::ZERO;
+
+        let mut min = Pos::ZERO;
+        let mut max = Pos::ZERO;
+
+        for DigStep { dir, meters, .. } in self.steps.iter().copied() {
+            let stride = dir * meters;
+            let next_pos = pos + stride;
+            pos = next_pos;
+
+            min = min.min(pos);
+            max = max.max(pos);
+        }
+
+        (min, max)
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct DigStep {
     pub dir: Dir,
     pub meters: i32,
-    pub color: Color,
+    pub alt: AltStep,
+}
+
+impl DigStep {
+    pub fn into_alt(self) -> Self {
+        Self {
+            dir: self.alt.dir,
+            meters: self.alt.meters,
+            alt: self.alt,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
+pub struct AltStep {
+    pub dir: Dir,
+    pub meters: i32,
 }
 
 // #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, grid::Tile)]
@@ -165,13 +202,290 @@ fn area_enclosed_by_dig_path(path: &DigPlan) -> u32 {
         .unwrap()
 }
 
+mod qt {
+    use super::*;
+
+    use quadtree_rs::{area::AreaBuilder, point::Point, Quadtree};
+
+    type Area = quadtree_rs::area::Area<i32>;
+
+    #[derive(Debug)]
+    pub struct RegionMap {
+        qt: Quadtree<i32, Option<Side>>,
+        min: Pos,
+        max: Pos,
+    }
+
+    impl RegionMap {
+        pub fn new(min: Pos, max: Pos) -> Self {
+            let xrange = max.x.abs_diff(min.x);
+            let yrange = max.y.abs_diff(min.y);
+
+            let range = xrange.max(yrange);
+
+            let depth = range.checked_ilog2().unwrap() + 1;
+
+            let mut qt = Quadtree::new(depth.try_into().unwrap());
+
+            qt.insert(Self::make_area_minmax(min, max), None);
+
+            Self { qt, min, max }
+        }
+
+        pub fn split(&mut self, cut_line_start: Pos, cut_line_end: Pos) {}
+
+        fn make_point(pos: Pos) -> Point<i32> {
+            Point { x: pos.x, y: pos.y }
+        }
+
+        fn make_area_minmax(min: Pos, max: Pos) -> Area {
+            let width = max.x - min.x + 1;
+            let height = max.y - min.y + 1;
+            AreaBuilder::default()
+                .anchor(Self::make_point(min))
+                .dimensions((width, height))
+                .build()
+                .unwrap()
+        }
+
+        fn make_areas_cut_line(&self, cut_line_start: Pos, cut_line_end: Pos) -> (Area, Area) {
+            let cut_line_dir = Dir::parallel_to(cut_line_end - cut_line_start)
+                .expect("cut line endpoints must be colinear");
+
+            let left = cut_line_dir.turn(Side::Left);
+            let right = cut_line_dir.turn(Side::Right);
+
+            let area_left =
+                self.make_area_cut_line(cut_line_start + left, cut_line_end + left, Side::Left);
+
+            let area_right =
+                self.make_area_cut_line(cut_line_start + right, cut_line_end + right, Side::Right);
+
+            (area_left, area_right)
+
+            // let line_min = cut_line_start.min(cut_line_end);
+            // let line_max = cut_line_start.max(cut_line_end);
+
+            // let (top, bot, left, right) = match cut_line_dir {
+            //     Dir::N | Dir::S => {
+            //         let top = line_min.y;
+            //         let bot = line_max.y;
+
+            //         let left = self.min.x;
+            //         let right = self.max.x;
+
+            //         (top, bot, left, right)
+            //     }
+            //     Dir::E | Dir::W => {
+            //         let top = self.min.y;
+            //         let bot = self.max.y;
+
+            //         let left = line_min.x;
+            //         let right = line_max.x;
+
+            //         (top, bot, left, right)
+            //     }
+            // };
+
+            // let top_left = Pos::new(left, top);
+            // let top_right = Pos::new(right, top);
+            // let bot_left = Pos::new(left, bot);
+            // let bot_right = Pos::new(right, bot);
+
+            // match cut_line_dir {
+            //     Dir::N => (
+            //         Self::make_area_minmax(top_left, line_max),
+            //         Self::make_area_minmax(line_min, bot_right),
+            //     ),
+            //     Dir::E => (
+            //         Self::make_area_minmax(top_left, line_max),
+            //         Self::make_area_minmax(line_min, bot_right),
+            //     ),
+            //     Dir::S => (
+            //         Self::make_area_minmax(line_min, bot_right),
+            //         Self::make_area_minmax(top_left, line_max),
+            //     ),
+            //     Dir::W => (
+            //         Self::make_area_minmax(line_min, bot_right),
+            //         Self::make_area_minmax(top_left, line_max),
+            //     ),
+            // }
+        }
+
+        // fn make_area_cut_line_and_dir(&self, line_a: Pos, line_b: Pos, dir: Dir) -> Area<i32> {
+        //     let line_min = line_a.min(line_b);
+        //     let line_max = line_a.max(line_b);
+
+        //     let (min, max) = match dir {
+        //         Dir::N => (Pos::new(line_min.x, self.min.y), Pos::new()),
+        //         Dir::E => {
+        //             line_c.x = self.max.x;
+        //             line_d.x = self.max.x;
+        //         }
+        //         Dir::S => {
+        //             line_c.y = self.max.y;
+        //             line_d.y = self.max.y;
+        //         }
+        //         Dir::W => {
+        //             line_c.x = self.min.x;
+        //             line_d.x = self.min.x;
+        //         }
+        //     };
+
+        //     let min = line_a.min();
+        // }
+
+        fn make_area_cut_line(&self, start: Pos, end: Pos, side: Side) -> Area {
+            use Dir::*;
+            use Side::*;
+
+            let dir = Dir::parallel_to(end - start).unwrap();
+
+            let line_min = start.min(end);
+            let line_max = start.max(end);
+
+            let (min, max) = match dir {
+                N | S => {
+                    let line_x = start.x;
+                    let min_y = line_min.y;
+                    let max_y = line_max.y;
+
+                    match (dir, side) {
+                        (N, Left) | (S, Right) => {
+                            // ........
+                            // ***|....
+                            // ***|....
+                            // ........
+                            (Pos::new(self.min.x, min_y), Pos::new(line_x, max_y))
+                        }
+
+                        (N, Right) | (S, Left) => {
+                            // ........
+                            // ...|****
+                            // ...|****
+                            // ........
+                            (Pos::new(line_x, min_y), Pos::new(self.max.x, max_y))
+                        }
+
+                        _ => unreachable!(),
+                    }
+                }
+                E | W => {
+                    let line_y = start.y;
+                    let min_x = line_min.x;
+                    let max_x = line_max.x;
+
+                    match (dir, side) {
+                        (E, Left) | (W, Right) => {
+                            // ..****..
+                            // ..----..
+                            // ........
+                            // ........
+                            (Pos::new(min_x, self.min.y), Pos::new(max_x, line_y))
+                        }
+
+                        (E, Right) | (W, Left) => {
+                            // ........
+                            // ..----..
+                            // ..****..
+                            // ..****..
+                            (Pos::new(min_x, line_y), Pos::new(max_x, self.max.y))
+                        }
+
+                        _ => unreachable!(),
+                    }
+                }
+            };
+        }
+
+        // precondition: no part of `cut` escapes `from`
+        fn cut_area_out(from: Area, cut: Area) -> SmallVec<[Area; 4]> {
+            /*
+              a  b   c  d
+              +--+---+--+
+              |         |
+             e+ f+---+g +h
+              |  |   |  |
+             i+ j+---+k +l
+              |         |
+              +--+---+--+
+              m  n   o  p
+            */
+
+            try_area(a, f);
+            try_area(b, g);
+            try_area(c, h);
+            try_area(e, j);
+            try_area(g, l);
+            try_area(i, n);
+            try_area(j, o);
+            try_area(k, p);
+
+            let (from_min, from_max) = Self::area_minmax(&from);
+            let (cut_min, cut_max) = Self::area_minmax(&cut);
+
+            let four_corners =
+                |min: Pos, max: Pos| (min, Pos::new(max.x, min.y), Pos::new(min.x, max.y), max);
+
+            let (a, d, m, p) = four_corners(from_min, from_max);
+            let (f, g, j, k) = four_corners(cut_min, cut_max);
+
+            let b = Pos::new(f.x, a.y);
+            let c = Pos::new(k.x, a.y);
+
+            let n = Pos::new(f.x, p.y);
+            let o = Pos::new(k.x, p.y);
+
+            let e = Pos::new(a.x, f.y);
+            let i = Pos::new(a.x, k.y);
+
+            let h = Pos::new(p.x, f.y);
+            let l = Pos::new(p.x, k.y);
+
+            let mut remaining_areas = SmallVec::new();
+
+            let mut try_area = |min: Pos, max: Pos| {
+                if min != max {
+                    remaining_areas.push(Self::make_area_minmax(min, max));
+                }
+            };
+
+            ()
+        }
+
+        fn area_minmax(area: &Area) -> (Pos, Pos) {
+            let min = area.anchor();
+            let min = Pos { x: min.x, y: min.y };
+            let size = Pos {
+                x: area.width(),
+                y: area.height(),
+            };
+
+            let max = min + size - Pos::ONE;
+
+            (min, max)
+        }
+    }
+}
+use qt::RegionMap;
+
+fn area_enclosed_by_dig_path_fast(path: &DigPlan) -> u32 {
+    let (min, max) = path.min_and_max_positions();
+
+    let mut regions = RegionMap::new(min, max);
+
+    0
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let plan = parsing::parse_input(input);
 
     Some(area_enclosed_by_dig_path(&plan))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u32> {
+    let plan = parsing::parse_input(input).into_alt();
+
     None
 }
 
@@ -190,24 +504,39 @@ mod parsing {
         .parse(input)
     }
 
-    fn hex_digit2(input: &str) -> IResult<&str, u8> {
-        take_while_m_n(2, 2, |c: char| c.is_hex_digit())
-            .map(|hex2| u8::from_str_radix(hex2, 16).expect("input already validated"))
-            .parse(input)
+    fn hex_digits<'a>(count: usize) -> impl FnMut(&'a str) -> IResult<&'a str, u8> {
+        move |input: &str| {
+            take_while_m_n(count, count, |c: char| c.is_hex_digit())
+                .map(|hex| u8::from_str_radix(hex, 16).expect("input already validated"))
+                .parse(input)
+        }
     }
 
-    fn color(input: &str) -> IResult<&str, Color> {
-        let color = preceded(
+    fn alt_step(input: &str) -> IResult<&str, AltStep> {
+        let alt_step = preceded(
             char('#'),
-            tuple((hex_digit2, hex_digit2, hex_digit2)).map(|(r, g, b)| Color { r, g, b }),
+            tuple((hex_digits(5), hex_digits(1))).map_res(|(meters, dir)| {
+                // 0 means R, 1 means D, 2 means L, and 3 means U.
+                let dir = match dir {
+                    0 => Dir::E,
+                    1 => Dir::S,
+                    2 => Dir::W,
+                    3 => Dir::N,
+                    _ => return Err(()),
+                };
+                Ok(AltStep {
+                    dir,
+                    meters: meters.into(),
+                })
+            }),
         );
 
-        delimited(char('('), color, char(')')).parse(input)
+        delimited(char('('), alt_step, char(')')).parse(input)
     }
 
     fn step(input: &str) -> IResult<&str, DigStep> {
-        ws_tuple((dir, decimal_number, color))
-            .map(|(dir, meters, color)| DigStep { dir, meters, color })
+        ws_tuple((dir, decimal_number, alt_step))
+            .map(|(dir, meters, alt)| DigStep { dir, meters, alt })
             .parse(input)
     }
 
